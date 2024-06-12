369d160891b8e0fabd59440b39fae600e8e103f8
Jauadul15 <rashik.gmh@gmail.com>
2024-06-12 14:23:32 +0600

    area of the circle and volume of the Cylinder math

--- math2.c
+++ math2.c
@@ -1,10 +1,22 @@
 #include<stdio.h>
+#include<math.h>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 int main(){
-    int radius=2;
-    float pi=3.1416;
-    printf("the area of the circle is %f",pi*radius*radius);
-    int height = 2;
-    printf("volume of the circle is %f",pi*radius*radius*height);
+    float radius,area,height,volume;
+    printf("Enter the radius of the circle: \n");
+    scanf("%f",&radius);
+
+    area = M_PI*radius*radius;
+    printf("the area of the circle is %.2f\n",area);
+
+    printf("Enter the Height of the cylinder: \n");
+    scanf("%f",&height);
+
+    volume = M_PI*radius*radius*height;
+    printf("The volume of the cylinder is %.2f",volume);
     return 0;
 }
