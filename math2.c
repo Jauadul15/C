5164e58ed10f60263362db47eabf7a2daa700396
Jauadul15 <rashik.gmh@gmail.com>
2024-06-12 13:54:46 +0600

    Calculate the area of circle and volume of a cylender

---
+++ math2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int radius=2;
+    float pi=3.1416;
+    printf("the area of the circle is %f",pi*radius*radius);
+    int height = 2;
+    printf("volume of the circle is %f",pi*radius*radius*height);
+    return 0;
+}
