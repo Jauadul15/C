c5c72ac6034a7087b3230b718c7f1cc9a1e945af
Jauadul15 <rashik.gmh@gmail.com>
2024-07-01 18:43:07 +0600

    multiplication and their sum math

--- loopmath3.c
+++ loopmath3.c
@@ -2,11 +2,15 @@
 
 int main(){
     int multiplier=10;
+    int sum=0;
     printf("Multiplication of %d\n",multiplier);
     for (int i =1; i<=10; i++)
-    {
+    {   
+        int allSum = multiplier*i;
         printf("%d x %d = %d\n",multiplier,i,multiplier*i);
+        sum+=allSum;
     }
+    printf("all sum = %d",sum);
     
     return 0;
 }
