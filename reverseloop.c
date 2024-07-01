e897889627be085f2aff865459d3f39ad549e5cb
Jauadul15 <rashik.gmh@gmail.com>
2024-07-01 14:22:51 +0600

    loop practise normal method reverse decrement

---
+++ reverseloop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int i,n;
+    printf("Enter the value\n");
+    scanf("%d",&n);
+    for ( i = n; i ; i--)
+    {
+        printf("the value is %d\n",i);
+    }
+    
+    return 0;
+}
